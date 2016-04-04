92141dfc546a61ea5080c0758d3055219f4b552e
jyrienstra <joukerienstra@hotmail.com>
2016-04-04 20:14:49 +0200

    main and simulator currently split. This can be merged later on. For testing purposes it has been split.

--- src/main/Simulator.java
+++ src/main/Simulator.java
@@ -8,8 +8,14 @@ import src.controller.SimController;
 public class Simulator {
 
     public static SimController sim;
+    public static Main main;
 
     public Simulator(){
         sim = new SimController();
+
+        main = new Main();
+        //TEMPORARY split between main and SimController can be merged later on.
+        //For testing purposes it has been split
+        // Comment a line out to test one or the other
     }
 }
