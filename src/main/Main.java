53bd1b0be15f633cecc1d6c3727c33b7151999c8
jyrienstra <joukerienstra@hotmail.com>
2016-04-05 12:07:18 +0200

    Added buttons. Actions currently do not call the correct views.(see issue in github)

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,10 +7,16 @@ import src.view.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import static src.main.Simulator.sim;
+
 public class Main {
     private JFrame screen = new JFrame("MVC test with tabbed pane");
     private TestView testView;
     private CarParkView carParkView;
+    private JButton button1 = new JButton("One step");
+    private JButton button2 = new JButton("100 steps");
+
+
 
 
     public Main() {
@@ -36,7 +42,28 @@ public class Main {
 
         //create a new carparkview and add this to tab 2
         carParkView = new CarParkView(3, 6, 30);
+        JPanel jp2Wrapper = new JPanel(); //wrapper to add the 2 buttons
+        jp2Wrapper.setLayout(new BoxLayout(jp2Wrapper, BoxLayout.PAGE_AXIS));
+        jp2Wrapper.add(button1);
+        jp2Wrapper.add(Box.createRigidArea(new Dimension(0,5)));
+        jp2Wrapper.add(button2); //@bug buttons not resizing correctly when resizing the window
+        jp2Wrapper.setBorder(BorderFactory.createEmptyBorder(10,10,10,10)); //add a border
+        jp2.add(jp2Wrapper, BorderLayout.WEST);
         jp2.add(carParkView, BorderLayout.CENTER);
+
+        //Add actions to buttons
+        button1.addActionListener(e -> {
+            // ^ We used a lambda as a shortcut to create an anonymous inner class
+            sim.run(1); // We imported the static Simulator (SimController object) to call this object's run method 1 time.
+        });
+        button2.addActionListener(e -> {
+            // ^ We used a lambda as a shortcut to create an anonymous inner class
+            sim.run(100); // We imported the static Simulator (SimController object) to call this object's run method 100 times.
+        });
+        //@todo currently does not work because simview.tick is called in controller and not abstractview.tick
+        //@todo this has to be fixed by adding the views to a collection and looping over it to update the views.
+        //@todo the abstractview has to make sure tick and updateview is in the subclasses
+
 
         //create a new testview and add this to tab 3
         testView = new TestView(3, 6, 30);
