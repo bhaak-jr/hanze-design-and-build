fdadf3161ad0e38ffa86c22f9484bb1656ce7da9
Bas Haaksema <b.haaksema@hotmail.com>
2016-04-05 22:37:47 +0200

    Added new views

---
+++ src/view/TextView.java
@@ -0,0 +1,19 @@
+package src.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+import src.model.*;
+
+/**
+ * Created by Bas Haaksema on 05-Apr-16.
+ */
+public class TextView extends AbstractView {
+
+    /**
+     * Constructor for objects of class CarPark
+     */
+    public TextView(Model model) {
+        super(model);
+    }
+}
