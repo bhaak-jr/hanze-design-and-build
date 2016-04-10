269766dd3a9a44dbc28f227221d8f25ea345aec2
Bas Haaksema <b.haaksema@hotmail.com>
2016-04-10 20:54:27 +0200

    Added a seperate controller for the caption and reservation button

---
+++ src/controller/CarParkController.java
@@ -0,0 +1,44 @@
+package src.controller;
+
+import javax.swing.*;
+import java.awt.*;
+import src.model.*;
+
+/**
+ * Controller subclass.
+ * Created by Bas Haaksema on 10-Apr-16.
+ */
+public class CarParkController extends AbstractController {
+
+    public CarParkController(CarParkModel carParkModel) {
+        super(carParkModel);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); //vertical layout
+
+        JLabel textRegular = new JLabel("NORMAL");
+        JLabel textParking = new JLabel("PASSHOLDER");
+        JLabel textBadParking = new JLabel("BAD PARKER");
+        JLabel textReservation = new JLabel("RESERVATION");
+
+        textRegular.setForeground(Color.red);
+        textParking.setForeground(Color.blue);
+        textBadParking.setForeground(Color.green);
+        textReservation.setForeground(Color.orange);
+
+        textRegular.setAlignmentX(Component.CENTER_ALIGNMENT);
+        textParking.setAlignmentX(Component.CENTER_ALIGNMENT);
+        textBadParking.setAlignmentX(Component.CENTER_ALIGNMENT);
+        textRegular.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        add(textRegular);
+        add(textParking);
+        add(textBadParking);
+        add(textReservation);
+
+        JButton reserveButton = new JButton("Reserve spot"); // Create Button
+        reserveButton.addActionListener(e -> carParkModel.reserve()); // Add Listener
+        reserveButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(reserveButton); // Add to JPanel
+
+        setVisible(true);
+    }
+}
